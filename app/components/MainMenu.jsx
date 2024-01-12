277ed56db10abe47a00c92dce161e133de617cb6
James <jjguevarag@gmail.com>
2024-01-12 12:08:48 -0600

    fix links nav

--- app/components/MainMenu.jsx
+++ app/components/MainMenu.jsx
@@ -1,17 +1,18 @@
 
 import Link from "next/link"
+
 function MainMenu() {
     return (
-        <>
-            <nav className="main-nav" id="main-nav">
-                <ul className="main-menu">
-                <li className="main-menu__item"><Link to="/" className="main-menu__link">Inicio</Link></li>
-                    <li className="main-menu__item"><Link to="../ProjectsPage/page.jsx" className="main-menu__link">Proyectos</Link></li>
-                    <li className="main-menu__item"><Link to="../ExperiencePage/page.jsx" className="main-menu__link">Experiencias</Link></li>
-                    <li className="main-menu__item"><Link to="../SkillsPage/page.jsx" className="main-menu__link">Habilidades</Link></li>
-                </ul>
-            </nav>
-        </>
+    <>
+    <nav className="main-nav" id="main-nav">
+        <ul className="main-menu">
+            <li className="main-menu__item"><Link href="/" className="main-menu__link">Inicio</Link></li>
+            <li className="main-menu__item"><Link href="/ProjectsPage" className="main-menu__link">Proyectos</Link></li>
+            <li className="main-menu__item"><Link href="/ExperiencePage" className="main-menu__link">Experiencias</Link></li>
+            <li className="main-menu__item"><Link href="/SkillsPage" className="main-menu__link">Habilidades</Link></li>
+        </ul>
+    </nav>
+    </>
     )
 }
 
